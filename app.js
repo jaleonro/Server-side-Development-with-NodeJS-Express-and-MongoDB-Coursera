fac8ec466871304029c9e56dd0ce7d5fd49e4c69
Johnathan Andrés León Rodriguez <Johnathan.Leon@ibm.com>
2020-12-24 20:12:26 -0500

    File Upload

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 var passport = require('passport');
 var authenticate = require('./authenticate');
@@ -53,6 +54,7 @@ app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
