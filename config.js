0637484342ffb380fe68aca9247beda0c7b4bf54
Johnathan Andrés León Rodriguez <Johnathan.Leon@ibm.com>
2020-12-27 21:33:34 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '404476550997837',
+        clientSecret: '64f7e25cf578f5438bee07f905344cdc'
+    }
 }
